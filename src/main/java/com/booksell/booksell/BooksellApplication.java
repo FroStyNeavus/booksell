2f62d04e2967b34232128481918cb3ad28aa16ea
Mu Tian Wang <mutian.wang@hotmail.com>
2020-06-05 21:57:47 -0400

    Initialise Booksell project with Spring Boot

---
+++ src/main/java/com/booksell/booksell/BooksellApplication.java
@@ -0,0 +1,13 @@
+package com.booksell.booksell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booksell/booksell/BooksellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksell.booksell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
